255b111f678841b8aa4eae3205e26e7cdbace33e
Saponja <jsaponjic512@gmail.com>
2021-03-21 12:52:18 +0100

    Dokumentovan interfejs Radnja i klasa VulkanizerskaRadnja koja implementira taj interfejes

---
+++ src/auto_radnja/Radnja.java
@@ -0,0 +1,43 @@
+package auto_radnja;
+
+import java.util.LinkedList;
+
+import auto_radnja.gume.AutoGuma;
+
+/**
+ * 
+ * Interfejs koji opisuje ponasanje radnje za auto gume.
+ * 
+ * 
+ * @author Jovan Saponjic
+ * @version 0.1
+ *
+ */
+
+public interface Radnja {
+	
+	/**
+	 * Dodavanje nove gume u listu
+	 *
+	 * 
+	 * @param a Objekat klase AutoGuma
+	 * @throws java.lang.NullPointerException ako je guma null
+	 * @throws java.lang.RuntimeException ako guma vec postoji u listi
+	 */
+	void dodajGumu(AutoGuma a);
+	/**
+	 * Pronalazenje gume po parametru markaModel.
+	 * 
+	 * @param markaModel Marka i model gume kao string
+	 * @return
+	 * 	<ul>
+	 * 		<li> Listu auto guma cije se marka i model poklapaju sa kriterijumom pretrage </li>
+	 * 		<li> Null ako je prosledjeni parametar null</li>
+	 * 	</ul>
+	 * 
+	 * 
+	 * 
+	 */
+	LinkedList<AutoGuma> pronadjiGumu(String markaModel);
+
+}
